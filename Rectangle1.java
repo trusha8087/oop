30d3e2f322b4775a2f678d083623c33fc4f7ce4d
trusha8087 <trusha.8087@gmail.com>
2026-01-31 17:44:43 +0530

    Add files via upload

---
+++ Rectangle1.java
@@ -0,0 +1,44 @@
+class Rectangle1 {
+
+    double width;
+    double height;
+
+    double getArea() {
+        return width * height;
+    }
+
+    double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enrollnment no. 240390107034");
+        Rectangle1 r1 = new Rectangle1();
+        r1.width = 4;
+        r1.height = 40;
+
+        Rectangle1 r2 = new Rectangle1();
+        r2.width = 3.5;
+        r2.height = 35.9;
+
+        System.out.println("Rectangle 1:");
+        System.out.println("Width: " + r1.width);
+        System.out.println("Height: " + r1.height);
+        System.out.println("Area: " + r1.getArea());
+        System.out.println("Perimeter: " + r1.getPerimeter());
+
+        System.out.println("\nRectangle 2:");
+        System.out.println("Width: " + r2.width);
+        System.out.println("Height: " + r2.height);
+        System.out.println("Area: " + r2.getArea());
+        System.out.println("Perimeter: " + r2.getPerimeter());
+
+        if (r1.getArea() > r2.getArea()) {
+            System.out.println("\nRectangle 1 has a larger area.");
+        } else if (r2.getArea() > r1.getArea()) {
+            System.out.println("\nRectangle 2 has a larger area.");
+        } else {
+            System.out.println("\nBoth rectangles have equal area.");
+        }
+    }
+}
