da27c4f68b1672dbaf443d0ec290827f55e2c8f7
trusha8087 <trusha.8087@gmail.com>
2026-03-06 21:12:19 +0530

    Add files via upload

---
+++ PasswordValidator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner; 
+ 
+public class PasswordValidator { 
+    public static boolean isValidPassword(String password) { 
+        if (password.length() < 8) { 
+            return false; 
+        } 
+        int digitCount = 0; 
+        for (int i = 0; i < password.length(); i++) { 
+            char ch = password.charAt(i); 
+            if (!Character.isLetterOrDigit(ch)) { 
+                return false; 
+            } 
+            if (Character.isDigit(ch)) { 
+                digitCount++; 
+            } 
+        } 
+        return digitCount >= 2; 
+    } 
+ 
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Trusha - 240390107034"); 
+        System.out.print("Enter Password: "); 
+        String password = sc.nextLine(); 
+        if (isValidPassword(password)) { 
+            System.out.println("Valid Password"); 
+        } else { 
+            System.out.println("Invalid Password"); 
+        }  
+    } 
+} 
